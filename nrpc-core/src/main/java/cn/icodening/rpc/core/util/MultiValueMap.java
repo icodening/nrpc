f4e047640b3b5c95e242455568cc27c912bf97ae
icodening <747167846@qq.com>
2021-03-10 01:26:18 +0800

    增加容器MultiValueMap, 增加Key大小写的容器CaseInsensitiveKeyMap

---
+++ nrpc-core/src/main/java/cn/icodening/rpc/core/util/MultiValueMap.java
@@ -0,0 +1,32 @@
+package cn.icodening.rpc.core.util;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author icodening
+ * @date 2021.03.09
+ */
+public interface MultiValueMap<K, V> extends Map<K, List<V>> {
+
+    V getFirst(K key);
+
+    void add(K key, V value);
+
+    void addAll(K key, List<? extends V> values);
+
+    void addAll(MultiValueMap<K, V> values);
+
+    default void addIfAbsent(K key, V value) {
+        if (!containsKey(key)) {
+            add(key, value);
+        }
+    }
+
+    void set(K key, V value);
+
+    void setAll(Map<K, V> values);
+
+    Map<K, V> toSingleValueMap();
+
+}
